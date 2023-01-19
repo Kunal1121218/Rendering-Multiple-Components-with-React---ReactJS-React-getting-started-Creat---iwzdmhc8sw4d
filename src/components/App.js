fb963fe45ae37d88c167b96976e2b694651aa4db
Kunal Jadhav <112310504+Kunal1121218@users.noreply.github.com>
2023-01-19 13:02:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
+const Project = ({ name, description }) => {
+  return (
+    <>
+      <p data-ns-test="project-name">{name}</p>{" "}
+      <p data-ns-test="project-description">{description}</p>
+    </>
+  );
+};
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
+    <div id="main">
+      <Project
+        name="E-Commerce"
+        description="Build an end-to-end E Commerce App"
+      />
 
+      <Project name="Netflix Clone" description="Build a new netflix clone" />
+
+      <Project
+        name="Game of thrones"
+        description="New game of thrones website for kahlisi"
+      />
+    </div>
+  );
+};
 
 export default App;
